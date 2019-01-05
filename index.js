3fa6a3b48d10d531c2617ddfb28b7a6432a9f833
Vladyslav Tertyshnyi <vladislafus@gmail.com>
2019-01-05 19:44:26 +0200

    Fix build issues

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 import { AppRegistry, UIManager } from 'react-native';
 
-import App from './src/app';
+import App from './src/App';
 import { name as appName } from './app.json';
 
 if (UIManager.setLayoutAnimationEnabledExperimental) {
